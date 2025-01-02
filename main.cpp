568735ab12d384ac4ea8bb9d263218bc731c52ac
Shwei <portal.gamy@gmail.com>
2025-01-02 22:50:45 +0200

    Initial project commit, OpenGL window

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(){
+
+//    std::cout << "gsajdhajsd" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "IvankaKekagit ", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
