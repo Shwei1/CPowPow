740dd9dc74d4065b03e139a45ead904c7630e68f
Shwei <portal.gamy@gmail.com>
2025-03-09 00:46:07 +0200

    structure added

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -xe 
 
-g++ -Wall -Wextra -o main ./main.cpp ./glad.c -Iinclude -I/opt/homebrew/include -L/opt/homebrew/lib -lglfw -framework OpenGL -DGL_SILENCE_DEPRECATION
+g++ -Wall -Wextra -o main src/main.cpp src/glad.c -Iinclude -I/opt/homebrew/include -L/opt/homebrew/lib -lglfw -framework OpenGL -DGL_SILENCE_DEPRECATION
 
